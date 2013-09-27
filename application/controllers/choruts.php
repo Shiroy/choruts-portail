543a239f0e859b172875eeb0c7084bdcc6efdd27
Shiroy <wacant@free.fr>
2013-09-27 22:52:48 +0200

    Ajout du carroussel

--- application/controllers/choruts.php
+++ application/controllers/choruts.php
@@ -8,6 +8,9 @@ class choruts extends CI_Controller
     
     public function index()
     {
+        $carroussel = $this->Index->getCarroussel();
+        
+        $this->twig->set("carroussel", $carroussel);
         $this->twig->render("index.html.twig");
     }
 }
---
+++ application/models/index.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of IndexModel
+ *
+ * @author antoine
+ */
+class Index extends CI_Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public function getCarroussel()
+    {
+        $carroussel = array();
+        
+        $titre = $this->db->query("SELECT content FROM site_string WHERE label='carroussel_titre'")->result();
+        $carroussel['titre'] = $titre[0]->content;
+        $contenue = $this->db->query("SELECT content FROM site_string WHERE label='carroussel_contenue'")->result();
+        $carroussel['contenue'] = $contenue[0]->content;
+        
+        return $carroussel;
+    }
+}
+
+?>
