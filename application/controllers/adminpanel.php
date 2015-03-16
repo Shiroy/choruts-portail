53f150f9c1be23c7adcf762723c6fc3510ad91c1
ddcctt <dylcota@hotmail.fr>
2015-03-16 19:35:47 +0100

    Update adminpanel.php

--- application/controllers/adminpanel.php
+++ application/controllers/adminpanel.php
@@ -119,7 +119,7 @@ class adminpanel extends MY_Controller
         if($page < 1)
             $page = 1;
         
-        define("USER_PER_PAGE", 40);
+        define("USER_PER_PAGE", 20);
         
         $users = $this->user->getUsers($page, USER_PER_PAGE);
         
